de936e21d494c289c8b5f83c2d3fd9ac79db4aea
Robbie <robbie@>
2024-08-01 12:42:26 +0900

    fix: first commit

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -120,7 +120,7 @@ public class CampManagementApplication {
     private static void displayMainView() throws InterruptedException {
         boolean flag = true;
         while (flag) {
-            System.out.println("\n==================================");
+            System.out.println("\n===================================");
             System.out.println("내일배움캠프 수강생 관리 프로그램 실행 중...");
             System.out.println("1. 수강생 관리");
             System.out.println("2. 점수 관리");
@@ -144,7 +144,7 @@ public class CampManagementApplication {
     private static void displayStudentView() {
         boolean flag = true;
         while (flag) {
-            System.out.println("==================================");
+            System.out.println("===================================");
             System.out.println("수강생 관리 실행 중...");
             System.out.println("1. 수강생 등록");
             System.out.println("2. 수강생 목록 조회");
@@ -186,7 +186,7 @@ public class CampManagementApplication {
     private static void displayScoreView() {
         boolean flag = true;
         while (flag) {
-            System.out.println("==================================");
+            System.out.println("===================================");
             System.out.println("점수 관리 실행 중...");
             System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
             System.out.println("2. 수강생의 과목별 회차 점수 수정");
