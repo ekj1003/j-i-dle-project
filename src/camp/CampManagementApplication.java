4b38d8f0e30b317a3fb5e499238cbd516725f0e3
inseooo0 <calmandpassion@korea.ac.kr>
2024-08-01 16:18:18 +0900

    feat: 특정 상태 수강생들의 필수 과목 평균 등급 조회 기능 추가

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -202,7 +202,9 @@ public class CampManagementApplication {
             System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
             System.out.println("2. 수강생의 과목별 회차 점수 수정");
             System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
-            System.out.println("4. 메인 화면 이동");
+            System.out.println("4. 특정 수강생 과목별 등급 조회");
+            System.out.println("5. 수강생의 특정 과목 회차별 등급 조회");
+            System.out.println("6. 메인 화면 이동");
             System.out.print("관리 항목을 선택하세요...");
             int input = sc.nextInt();
 
@@ -210,7 +212,8 @@ public class CampManagementApplication {
                 case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
                 case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
                 case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
-                case 4 -> flag = false; // 메인 화면 이동
+                case 5 -> inquireAvgGradeByMandatorySubject(); // 특정 상태 수강생들의 필수 과목 평균 등급 조회
+                case 6 -> flag = false; // 메인 화면 이동
                 default -> {
                     System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
                     flag = false;
@@ -286,4 +289,55 @@ public class CampManagementApplication {
         System.out.println("\n등급 조회 성공!");
     }
 
+    // 특정 상태 수강생들의 필수 과목 평균 등급 조회
+    private static void inquireAvgGradeByMandatorySubject() {
+
+        System.out.print("조회할 수강생의 상태를 입력하시오...");
+        String status = sc.next();
+        List<Student> students = studentStore.stream().
+                filter(s -> s.getStatus().equals(status)).toList();
+
+        System.out.println(status + " 상태 수강생들의 필수 과목 평균 등급을 조회합니다...");
+        System.out.println("====================================");
+
+        for (Student student : students) {
+            List<String> subjectList = student.getSubjectList();
+
+            for (String subjectId : subjectList) {
+                Subject subject = subjectStore.stream()
+                        .filter(s -> s.getSubjectId().equals(subjectId)).findFirst().get();
+
+                if (subject.getSubjectType().equals(SUBJECT_TYPE_MANDATORY)) {
+                    List<Integer> scoreList = scoreStore.stream().filter(s -> s.getStudentId().equals(student.getStudentId()) &&
+                            s.getSubjectId().equals(subjectId)).map(Score::getScore).toList();
+
+                    char avgGrade = getAvgGrade(scoreList);
+                    System.out.println(student.getStudentName() + " 수강생 " + subject.getSubjectName() + " 과목 평균 등급 = " + avgGrade);
+                }
+            }
+            System.out.println("====================================");
+        }
+    }
+
+    // 점수 리스트를 받아 평균 등급을 반환
+    private static char getAvgGrade(List<Integer> scoreList) {
+        int scoreSum = scoreList.stream().reduce(0, Integer::sum);
+        int avgSum = scoreSum / scoreList.size();
+        char avgGrade;
+
+        if (avgSum >= 95) {
+            avgGrade = 'A';
+        } else if (avgSum >= 90) {
+            avgGrade = 'B';
+        } else if (avgSum >= 80) {
+            avgGrade = 'C';
+        } else if (avgSum >= 70) {
+            avgGrade = 'D';
+        } else if (avgSum >= 60) {
+            avgGrade = 'F';
+        } else {
+            avgGrade = 'N';
+        }
+        return avgGrade;
+    }
 }
