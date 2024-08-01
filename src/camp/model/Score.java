34d91c22b3058d244a1b42e08d31ec4a66f76a76
inseooo0 <calmandpassion@korea.ac.kr>
2024-08-01 14:38:13 +0900

    feat: add template

--- src/camp/model/Score.java
+++ src/camp/model/Score.java
@@ -3,6 +3,7 @@ package camp.model;
 public class Score {
     private String scoreId;
     private String studentId;
+    private String subjectId;
     private int round;
     private int score;
     private char grade;
